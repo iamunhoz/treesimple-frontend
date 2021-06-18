fe054254c315c469f1fe891fea328671c596a801
Ivã Munhoz <iamunhoz@gmail.com>
2021-06-18 14:57:27 -0300

    natural-syntax files

--- src/data.tsx
+++ src/data.tsx
@@ -1,14 +1,1 @@
-interface config {
-  text: string;
-}
-
-const data: config = {
-  text: 'You do You'
-}
-
-function splitSentence(sentence: string): Array<string> {
-  return sentence.split('')
-}
-
-export { data }
-
+export {}
---
+++ src/p5/setup.tsx
@@ -0,0 +1,13 @@
+import p5Types from "p5"
+
+let setup = (p5: p5Types, canvasParentRef: Element) => {
+  let xyz = p5.createCanvas(500, 400).parent(canvasParentRef)
+  //Calculation to center the canvas 
+  let x = (p5.windowWidth - p5.width) / 2
+  let y = (p5.windowHeight - p5.height) / 2
+  xyz.position(x, y)
+  let inputer = p5.createInput('type something')
+  inputer.position((p5.windowWidth) / 2, 0)
+}
+
+export {setup}
