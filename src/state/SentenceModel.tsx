a6328950ed5cbf321e59f4b5a61a47469f3ad758
Ivã Munhoz <iamunhoz@gmail.com>
2023-03-30 08:22:15 -0300

    fix typings

--- src/state/SentenceModel.tsx
+++ src/state/SentenceModel.tsx
@@ -1,34 +1,34 @@
 import { TTree } from 'types/PhraseTypes'
 
 export const TreeModel: TTree = {
-  id: 1000,
+  id: '1000',
   fullSentence: 'Her holy trail is weakening',
   phrases: [
     {
-      id: 1,
+      id: '1',
       body: 'Her holy trail is weakening',
       head: null,
-      leftChildId: 2,
-      rightChildId: 3,
+      leftChildId: '2',
+      rightChildId: '3',
       parentId: null,
       type: 'Sentence'
     },
     {
-      id: 2,
+      id: '2',
       body: 'Her holy trail',
       head: null,
       leftChildId: null,
       rightChildId: null,
-      parentId: 1,
+      parentId: '1',
       type: 'NounP'
     },
     {
-      id: 3,
+      id: '3',
       body: 'is weakening',
       head: null,
       leftChildId: null,
       rightChildId: null,
-      parentId: 1,
+      parentId: '1',
       type: 'VerbP'
     }
   ]
