344b0f8ae0b7fbb5e86de69c3b837186ea3b5186
Ivã Munhoz <iamunhoz@gmail.com>
2023-04-24 19:58:19 -0300

    solving cors 1

--- src/services/axios/ApiSemToken.ts
+++ src/services/axios/ApiSemToken.ts
@@ -3,7 +3,12 @@ import { baseURL } from './ip'
 
 const ApiSemToken = axios.create({
   baseURL,
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  }
 })
 
 export default ApiSemToken
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
